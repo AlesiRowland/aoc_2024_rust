4a5fd64730cc9d10eb427d86df01fa8c10a294a7
AlesiRowland <alesi.rowland@essentia-analytics.com>
2024-12-02 12:01:50 +0000

    feat: day 2

---
+++ src/day_02.rs
@@ -0,0 +1,102 @@
+use std::cmp;
+use std::cmp::PartialEq;
+use clap::Parser;
+
+type Report = Vec<isize>;
+
+fn parse_input(input: &str) -> Vec<Report> {
+    input
+        .split('\n')
+        .map(|line| line.split_whitespace().map(|val| val.parse().unwrap()).collect())
+        .collect()
+}
+
+
+fn find_number_of_safe_reports(reports: &Vec<Report>) -> isize {
+    reports
+        .iter()
+        .map(|report| is_safe_report(report) as isize)
+        .sum()
+}
+
+
+fn is_safe_report(report: &Report) -> bool {
+    let mut index = 1;
+    let mut order = 0;
+
+    while index < report.len() {
+        let last_level = report.get(index - 1).unwrap();
+        let current_level = report.get(index).unwrap();
+        let diff = current_level.checked_sub(*last_level).unwrap();
+        let abs_diff = diff.abs();
+
+        if abs_diff < 1 || abs_diff > 3 {
+            return false;
+        };
+
+        let current_order = diff / abs_diff;
+
+        if order == 0 {
+            order = current_order
+        } else if order != current_order {
+            return false;
+        }
+
+        index += 1;
+    }
+    true
+}
+
+fn find_number_of_dampened_safe_reports(reports: &Vec<Report>) -> isize {
+    reports
+        .iter()
+        .map(|report| {
+            let sub_reports = create_sub_reports(report);
+            sub_reports.iter().any(is_safe_report) as isize
+        }).sum()
+}
+
+fn create_sub_reports(report: &Report) -> Vec<Report> {
+    (0..report.len())
+        .into_iter()
+        .map(|skipped| create_sub_report(skipped, report))
+        .collect()
+}
+
+fn create_sub_report(skipped: usize, report: &Report) -> Report {
+    let mut sub_report = Vec::new();
+    report
+        .iter()
+        .enumerate()
+        .for_each(|(index, value)| {
+            if index != skipped {
+
+            }
+        });
+    sub_report
+}
+#[cfg(test)]
+mod tests {
+    use crate::answers::{DAY_02_EASY, DAY_02_HARD};
+    use crate::day_02::{find_number_of_dampened_safe_reports, find_number_of_safe_reports, parse_input};
+
+    const INPUT: &str = include_str!("../resources/day_02/easy.txt");
+
+
+    #[test]
+    fn easy() {
+        let reports = parse_input(INPUT);
+        let left = find_number_of_safe_reports(&reports);
+        let right = DAY_02_EASY;
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn hard() {
+        let reports = parse_input(INPUT);
+        let left = find_number_of_dampened_safe_reports(&reports);
+        let right = DAY_02_HARD;
+        assert_eq!(left, right);
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day_01;
 mod answers;
+mod day_02;
 
 fn main() {
     println!("Hello, world!");
