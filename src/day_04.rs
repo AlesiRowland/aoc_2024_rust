bf851b8dcebbc81af288579389cc3b54ca8aba31
AlesiRowland <alesi.rowland@essentia-analytics.com>
2024-12-04 11:04:43 +0000

    fix: hard day 04 works

--- src/day_04.rs
+++ src/day_04.rs
@@ -4,11 +4,20 @@ use std::num::ParseIntError;
 
 type WordSearch = Vec<Vec<char>>;
 
+trait Matrix<T> {
+    fn get_scalar(&self, point: &Point) -> Option<&T>;
+}
+
+impl Matrix<char> for WordSearch {
+    fn get_scalar(&self, point: &Point) -> Option<&char> {
+        self.get(point.y)?.get(point.x)
+    }
+}
 fn parse_input(input: &str) -> WordSearch {
     input.split('\n').map(|l| l.chars().collect()).collect()
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct Point {
     x: usize,
     y: usize,
@@ -35,6 +44,11 @@ impl Point {
             }),
         }
     }
+    fn move_in_directions(&self, directions: &[Direction]) -> Option<Point> {
+        directions.iter().fold(Some(self.clone()), |a, b| {
+            a.and_then(|c| c.move_in_direction(b))
+        })
+    }
 }
 
 enum Direction {
@@ -60,44 +74,16 @@ fn iter_directions() -> [Vec<Direction>; 8] {
 fn get_word_count(word: &str, word_search: &WordSearch) -> usize {
     let word_chars = word.chars().collect::<Vec<_>>();
     let mut count = 0;
-    let mut all_points = Vec::new();
 
     for y in (0..word_search.len()) {
         for x in (0..word_search[0].len()) {
             let start = Point { x, y };
-
-            if let Point {x: 3, y: 9} = start {
-                println!("debug");
-            }
-
             for directions in iter_directions() {
-                let word_points =
-                    word_exists_in_direction(&start, &directions, &word_chars, &word_search);
-                if !word_points.is_empty() {
-                    count += 1;
-                    all_points.extend(word_points)
+                if word_exists_in_direction(&start, &directions, &word_chars, &word_search) {
+                    count += 1
                 }
-                // if word_exists_in_direction(&start, &directions, &word_chars, &word_search) {
-                //    count += 1
-                // }
             }
         }
-    }
-    println!("{:?}", all_points);
-
-
-    for y in (0..word_search.len()) {
-        for x in (0..word_search[0].len()) {
-            let point = Point {x, y};
-            let value;
-            if all_points.contains(&point) {
-                value = word_search[point.y][point.x];
-            } else {
-               value = '.';
-            }
-            print!("{}", value);
-        }
-        print!("\n");
     }
     count
 }
@@ -107,44 +93,110 @@ fn word_exists_in_direction(
     directions: &Vec<Direction>,
     word_chars: &Vec<char>,
     word_search: &WordSearch,
-) -> Vec<Point> {
-    let mut captured = Vec::new();
-    let mut current = start.clone();
+) -> bool {
+    let mut next_index = Some(start.clone());
 
     for char in word_chars {
+        let Some(current) = next_index.take() else {
+            return false;
+        };
 
-        captured.push(current.clone());
-        let Some(line) = word_search.get(current.y) else {
-            // return false;
-            return Vec::new();
+        let Some(ch) = word_search.get_scalar(&current) else {
+            return false;
         };
-        let Some(ch) = line.get(current.x) else {
-            // return false;
-            return Vec::new();
-        };
+        // let Some(line) = word_search.get(current.y) else {
+        //     return false;
+        // };
+        //
+        // let Some(ch) = line.get(current.x) else {
+        //     return false;
+        // };
+
         if char != ch {
-            // return false;
-            return Vec::new();
+            return false;
         };
+        next_index = current.move_in_directions(&directions);
+    }
+    true
+}
 
-        for direction in directions {
-            let Some(next_index) = current.move_in_direction(direction) else {
-                if captured.len() == word_chars.len() {
-                    return captured;
-                }
-                return Vec::new();
-                // return false;
-            };
-            current = next_index;
+fn get_cross_word_count(word_search: &WordSearch) -> usize {
+    let mut count = 0;
+
+    for y in (0..word_search.len()) {
+        for x in (0..word_search[0].len()) {
+            let start = Point { x, y };
+            if xmas_found(&start, &word_search) {
+                count += 1
+            }
+
         }
     }
-    // true
-    captured
+
+    count
+}
+
+fn xmas_found(start: &Point, word_search: &WordSearch) -> bool {
+    let Some(line) = word_search.get(start.y) else {
+        return false
+    };
+    let Some(origin) = line.get(start.x) else {
+        return false
+    };
+
+    if origin != &'A' {
+        return false
+    };
+
+    // Check left Diagonal
+    let Some(upper_left_index) = start.move_in_directions(&[Direction::North, Direction::West])
+    else {
+        return false
+    };
+    let Some(lower_right_index) = start.move_in_directions(&[Direction::South, Direction::East])
+    else {
+        return false
+    };
+
+    let Some(upper_left) = word_search.get_scalar(&upper_left_index) else {
+        return false
+    };
+    let Some(lower_right) = word_search.get_scalar(&lower_right_index) else {
+        return false
+    };
+
+    if !((upper_left == &'M' && lower_right == &'S') || (upper_left == &'S' && lower_right == &'M'))
+    {
+        return false
+    }
+
+    let Some(upper_right_index) = start.move_in_directions(&[Direction::North, Direction::East])
+    else {
+        return false
+    };
+    let Some(lower_left_index) = start.move_in_directions(&[Direction::South, Direction::West])
+    else {
+        return false
+    };
+
+    let Some(upper_right) = word_search.get_scalar(&upper_right_index) else {
+        return false
+    };
+    let Some(lower_left) = word_search.get_scalar(&lower_left_index) else {
+        return false
+    };
+
+    if !((upper_right == &'M' && lower_left == &'S') || (upper_right == &'S' && lower_left == &'M'))
+    {
+        return false
+    }
+    true
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_04::{get_word_count, parse_input};
+    use crate::answers::{DAY_04_EASY, DAY_04_HARD};
+    use crate::day_04::{get_cross_word_count, get_word_count, parse_input};
 
     const INPUT: &str = include_str!("../resources/day_04/easy.txt");
     const PREAMBLE: &str = include_str!("../resources/day_04/preamble.txt");
@@ -159,7 +211,15 @@ mod tests {
     fn easy() {
         let word_search = parse_input(INPUT);
         let left = get_word_count("XMAS", &word_search);
-        let right = 2406;
+        let right = DAY_04_EASY;
+        assert_eq!(left, right);
+    }
+
+    #[test]
+    fn hard() {
+        let word_search = parse_input(INPUT);
+        let left = get_cross_word_count(&word_search);
+        let right = DAY_04_HARD;
         assert_eq!(left, right);
     }
 }
